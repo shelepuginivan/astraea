bebb6bf85a0901c228ab2cb0a82c8ea2edd83b7c
shelepuginivan <shelepuginivanm@gmail.com>
2025-10-24 03:11:22 +0300

    feat(lib, natural): implement other methods of module

--- libastraea/src/natural/module.rs
+++ libastraea/src/natural/module.rs
@@ -1,9 +1,11 @@
 use std::{collections::HashSet, fmt::Display};
 
 use crate::core::{Instruction, InstructionError, Module};
+use crate::digit;
+use crate::math::Digit;
 use crate::math::sign::ToSign;
 use crate::natural::NaturalNumber;
-use crate::validate;
+use crate::validate::{self, ensure_args_count};
 
 pub struct NaturalModule {}
 
@@ -21,23 +23,113 @@ impl Module for NaturalModule {
     ) -> Result<Box<dyn Display>, InstructionError> {
         match instruction {
             Instruction::NaturalCompare => {
-                let (lhs, rhs) = validate::two_args::<NaturalNumber>(instruction, args)?;
+                let (lhs, rhs) = validate::two_args::<NaturalNumber>(&instruction, args)?;
                 Ok(Box::new(lhs.cmp(&rhs).to_sign()))
             }
 
             Instruction::NaturalIsZero => {
-                let n: NaturalNumber = validate::one_arg(instruction, args)?;
+                let n: NaturalNumber = validate::one_arg(&instruction, args)?;
                 Ok(Box::new(n.is_zero()))
             }
 
             Instruction::NaturalIncrement => {
-                let n: NaturalNumber = validate::one_arg(instruction, args)?;
+                let n: NaturalNumber = validate::one_arg(&instruction, args)?;
                 Ok(Box::new(n.inc()))
             }
 
             Instruction::NaturalAdd => {
-                let (lhs, rhs) = validate::two_args::<NaturalNumber>(instruction, args)?;
+                let (lhs, rhs) = validate::two_args::<NaturalNumber>(&instruction, args)?;
                 Ok(Box::new(lhs + rhs))
+            }
+
+            Instruction::NaturalSubtract => {
+                let (lhs, rhs) = validate::two_args::<NaturalNumber>(&instruction, args)?;
+
+                match lhs - rhs {
+                    Ok(res) => Ok(Box::new(res)),
+                    Err(err) => Err(InstructionError::new(err.message)),
+                }
+            }
+
+            Instruction::NaturalMultiplyByDigit => {
+                ensure_args_count(&instruction, &args, 2)?;
+
+                let lhs = validate::natural(&args[0])?;
+                let rhs = validate::digit(&args[1])?;
+
+                Ok(Box::new(lhs * rhs))
+            }
+
+            Instruction::NaturalMultiplyByPowerOfTen => {
+                ensure_args_count(&instruction, &args, 2)?;
+
+                let lhs = validate::natural(&args[0])?;
+                let rhs = validate::usize(&args[1])?;
+
+                Ok(Box::new(lhs.times_pow10(rhs)))
+            }
+
+            Instruction::NaturalMultiply => {
+                let (lhs, rhs) = validate::two_args::<NaturalNumber>(&instruction, args)?;
+                Ok(Box::new(lhs * rhs))
+            }
+
+            Instruction::NaturalSubtractMultipliedByDigit => {
+                validate::ensure_args_count(&instruction, &args, 3)?;
+
+                let lhs = validate::natural(&args[0])?;
+                let rhs = validate::natural(&args[1])?;
+                let digit = validate::digit(&args[2])?;
+
+                match lhs - rhs * digit {
+                    Ok(v) => Ok(Box::new(v)),
+                    Err(e) => Err(InstructionError::new(e.message)),
+                }
+            }
+
+            Instruction::NaturalCalculateDivisionDigit => {
+                validate::ensure_args_count(&instruction, &args, 3)?;
+
+                let lhs = validate::natural(&args[0])?;
+                let rhs = validate::natural(&args[1])?;
+                let k = validate::usize(&args[2])?;
+
+                if k + rhs.len() > lhs.len() {
+                    return Ok(Box::new(digit!(0)));
+                }
+
+                let lhs_digit = lhs.lsd_at(k + rhs.len() - 1);
+                let rhs_digit = rhs.msd_at(0);
+
+                Ok(Box::new(lhs_digit / rhs_digit))
+            }
+
+            Instruction::NaturalQuotient => {
+                let (lhs, rhs) = validate::two_args::<NaturalNumber>(&instruction, args)?;
+
+                match lhs / rhs {
+                    Ok(v) => Ok(Box::new(v)),
+                    Err(e) => Err(InstructionError::new(e.message)),
+                }
+            }
+
+            Instruction::NaturalRemainder => {
+                let (lhs, rhs) = validate::two_args::<NaturalNumber>(&instruction, args)?;
+
+                match lhs % rhs {
+                    Ok(v) => Ok(Box::new(v)),
+                    Err(e) => Err(InstructionError::new(e.message)),
+                }
+            }
+
+            Instruction::NaturalGCD => {
+                let (lhs, rhs) = validate::two_args::<NaturalNumber>(&instruction, args)?;
+                Ok(Box::new(lhs.gcd(rhs)))
+            }
+
+            Instruction::NaturalLCM => {
+                let (lhs, rhs) = validate::two_args::<NaturalNumber>(&instruction, args)?;
+                Ok(Box::new(lhs.lcm(rhs)))
             }
 
             _ => Err(InstructionError::new(format!(
