53b2ee28b7f9cee28a8b379cf4cd03e3f22504f7
shelepuginivan <shelepuginivanm@gmail.com>
2025-10-26 01:59:18 +0300

    chore(cli): add short about

--- astraea/src/cli/args.rs
+++ astraea/src/cli/args.rs
@@ -1,7 +1,7 @@
 use clap::Parser;
 
 #[derive(Parser, Debug)]
-#[command(version, about, long_about = None)]
+#[command(version, about = "astraea - Computer algebra system")]
 pub struct Args {
     /// Instruction to be called.
     #[arg(short, long)]
