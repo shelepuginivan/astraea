f275725f0a3dbb89e2a9eb06d6b167a5315f2539
shelepuginivan <shelepuginivanm@gmail.com>
2025-10-25 16:59:39 +0300

    feat(lib, integer): implement integer module

--- libastraea/src/integer/mod.rs
+++ libastraea/src/integer/mod.rs
@@ -1,3 +1,5 @@
 pub mod integer;
+pub mod module;
 
 pub use integer::*;
+pub use module::*;
---
+++ libastraea/src/integer/module.rs
@@ -0,0 +1,119 @@
+use std::{collections::HashSet, fmt::Display};
+
+use crate::core::{Instruction, InstructionError, Module};
+use crate::integer::Integer;
+use crate::natural::NaturalNumber;
+use crate::validate::{one_arg, two_args};
+
+pub struct IntegerModule {}
+
+impl IntegerModule {
+    pub fn new() -> Self {
+        IntegerModule {}
+    }
+}
+
+impl Module for IntegerModule {
+    fn process_instruction(
+        &self,
+        instruction: Instruction,
+        args: Vec<String>,
+    ) -> Result<Box<dyn Display>, InstructionError> {
+        match instruction {
+            Instruction::IntegerAbs => {
+                let v: Integer = one_arg(&instruction, args)?;
+
+                Ok(Box::new(v.abs()))
+            }
+
+            Instruction::IntegerSgn => {
+                let v: Integer = one_arg(&instruction, args)?;
+
+                Ok(Box::new(v.abs()))
+            }
+
+            Instruction::IntegerNeg => {
+                let v: Integer = one_arg(&instruction, args)?;
+
+                Ok(Box::new(-v))
+            }
+
+            Instruction::IntegerFromNatural => {
+                let v: NaturalNumber = one_arg(&instruction, args)?;
+
+                Ok(Box::new(Integer::from_natural(v)))
+            }
+
+            Instruction::IntegerToNatural => {
+                let v: Integer = one_arg(&instruction, args)?;
+
+                match v.to_natural() {
+                    Ok(v) => Ok(Box::new(v)),
+                    Err(e) => Ok(Box::new(InstructionError::new(e.message))),
+                }
+            }
+
+            Instruction::IntegerAdd => {
+                let (lhs, rhs) = two_args::<Integer>(&instruction, args)?;
+
+                Ok(Box::new(lhs + rhs))
+            }
+
+            Instruction::IntegerSubtract => {
+                let (lhs, rhs) = two_args::<Integer>(&instruction, args)?;
+
+                Ok(Box::new(lhs - rhs))
+            }
+
+            Instruction::IntegerMultiply => {
+                let (lhs, rhs) = two_args::<Integer>(&instruction, args)?;
+
+                Ok(Box::new(lhs * rhs))
+            }
+
+            Instruction::IntegerQuotient => {
+                let (lhs, rhs) = two_args::<Integer>(&instruction, args)?;
+
+                match lhs / rhs {
+                    Ok(v) => Ok(Box::new(v)),
+                    Err(e) => Ok(Box::new(InstructionError::new(e.message))),
+                }
+            }
+
+            Instruction::IntegerRemainder => {
+                let (lhs, rhs) = two_args::<Integer>(&instruction, args)?;
+
+                match lhs % rhs {
+                    Ok(v) => Ok(Box::new(v)),
+                    Err(e) => Ok(Box::new(InstructionError::new(e.message))),
+                }
+            }
+
+            _ => Err(InstructionError::new(format!(
+                "unknown instruction: {:?}",
+                instruction
+            ))),
+        }
+    }
+
+    fn implements(&self, instruction: Instruction) -> bool {
+        self.methods().contains(&instruction)
+    }
+
+    fn methods(&self) -> HashSet<Instruction> {
+        let mut method_set: HashSet<Instruction> = HashSet::new();
+
+        method_set.insert(Instruction::IntegerAbs);
+        method_set.insert(Instruction::IntegerSgn);
+        method_set.insert(Instruction::IntegerNeg);
+        method_set.insert(Instruction::IntegerFromNatural);
+        method_set.insert(Instruction::IntegerToNatural);
+        method_set.insert(Instruction::IntegerAdd);
+        method_set.insert(Instruction::IntegerSubtract);
+        method_set.insert(Instruction::IntegerMultiply);
+        method_set.insert(Instruction::IntegerQuotient);
+        method_set.insert(Instruction::IntegerRemainder);
+
+        method_set
+    }
+}
