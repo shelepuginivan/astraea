49109130fda9a680d632e614fb1b209c02c04698
shelepuginivan <shelepuginivanm@gmail.com>
2025-10-25 18:56:01 +0300

    feat(lib, core): add `ModuleGroup` composite

--- libastraea/src/core/module.rs
+++ libastraea/src/core/module.rs
@@ -18,3 +18,57 @@ pub trait Module {
     /// Returns all instructions implemented by the module.
     fn methods(&self) -> HashSet<Instruction>;
 }
+
+pub struct ModuleGroup {
+    modules: Vec<Box<dyn Module>>,
+}
+
+/// ModuleGroup is a composite for modules.
+impl ModuleGroup {
+    pub fn new(modules: Vec<Box<dyn Module>>) -> Self {
+        Self { modules }
+    }
+
+    pub fn add_module(&mut self, module: Box<dyn Module>) {
+        self.modules.push(module);
+    }
+}
+
+impl Module for ModuleGroup {
+    fn process_instruction(
+        &self,
+        instruction: Instruction,
+        args: Vec<String>,
+    ) -> Result<Box<dyn Display>, InstructionError> {
+        for module in &self.modules {
+            if module.implements(instruction) {
+                return module.process_instruction(instruction, args);
+            }
+        }
+
+        Err(InstructionError::new(format!(
+            "unknown instruction: {:?}",
+            instruction
+        )))
+    }
+
+    fn implements(&self, instruction: Instruction) -> bool {
+        for module in &self.modules {
+            if module.implements(instruction) {
+                return true;
+            }
+        }
+
+        false
+    }
+
+    fn methods(&self) -> HashSet<Instruction> {
+        let mut method_set: HashSet<Instruction> = HashSet::new();
+
+        for module in &self.modules {
+            method_set.extend(&module.methods());
+        }
+
+        method_set
+    }
+}
