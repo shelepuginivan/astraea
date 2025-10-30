158c092231212cfaf5ac795badb8cbc6df8a4961
shelepuginivan <shelepuginivanm@gmail.com>
2025-10-30 05:58:57 +0300

    test(lib, math): add more tests for `Digit`

--- libastraea/src/math/digit.rs
+++ libastraea/src/math/digit.rs
@@ -105,8 +105,7 @@ impl Digit {
     /// ```
     /// use libastraea::math::Digit;
     ///
-    /// let d = Digit::new(4).unwrap();
-    /// assert_eq!(d.to_char(), '4');
+    /// assert_eq!(Digit::Four.char(), '4');
     /// ```
     pub fn char(&self) -> char {
         match self {
@@ -304,6 +303,38 @@ mod tests {
     use std::cmp::Ordering;
 
     #[test]
+    fn test_digit_new() {
+        assert_eq!(Digit::new(0).unwrap(), Digit::Zero);
+        assert_eq!(Digit::new(1).unwrap(), Digit::One);
+        assert_eq!(Digit::new(2).unwrap(), Digit::Two);
+        assert_eq!(Digit::new(3).unwrap(), Digit::Three);
+        assert_eq!(Digit::new(4).unwrap(), Digit::Four);
+        assert_eq!(Digit::new(5).unwrap(), Digit::Five);
+        assert_eq!(Digit::new(6).unwrap(), Digit::Six);
+        assert_eq!(Digit::new(7).unwrap(), Digit::Seven);
+        assert_eq!(Digit::new(8).unwrap(), Digit::Eight);
+        assert_eq!(Digit::new(9).unwrap(), Digit::Nine);
+
+        assert!(Digit::new(28).is_err());
+    }
+
+    #[test]
+    fn test_digit_from_char() {
+        assert_eq!(Digit::from_char('0').unwrap(), Digit::Zero);
+        assert_eq!(Digit::from_char('1').unwrap(), Digit::One);
+        assert_eq!(Digit::from_char('2').unwrap(), Digit::Two);
+        assert_eq!(Digit::from_char('3').unwrap(), Digit::Three);
+        assert_eq!(Digit::from_char('4').unwrap(), Digit::Four);
+        assert_eq!(Digit::from_char('5').unwrap(), Digit::Five);
+        assert_eq!(Digit::from_char('6').unwrap(), Digit::Six);
+        assert_eq!(Digit::from_char('7').unwrap(), Digit::Seven);
+        assert_eq!(Digit::from_char('8').unwrap(), Digit::Eight);
+        assert_eq!(Digit::from_char('9').unwrap(), Digit::Nine);
+
+        assert!(Digit::from_char('a').is_err());
+    }
+
+    #[test]
     fn test_digit_cmp() {
         let lhs = Digit::new(9).unwrap();
         let rhs = Digit::new(8).unwrap();
@@ -324,15 +355,92 @@ mod tests {
             assert_eq!(digit!(v), Digit::new(v).unwrap())
         }
 
-        assert_eq!(digit!(0), Digit::new(0).unwrap());
-        assert_eq!(digit!(1), Digit::new(1).unwrap());
-        assert_eq!(digit!(2), Digit::new(2).unwrap());
-        assert_eq!(digit!(3), Digit::new(3).unwrap());
-        assert_eq!(digit!(4), Digit::new(4).unwrap());
-        assert_eq!(digit!(5), Digit::new(5).unwrap());
-        assert_eq!(digit!(6), Digit::new(6).unwrap());
-        assert_eq!(digit!(7), Digit::new(7).unwrap());
-        assert_eq!(digit!(8), Digit::new(8).unwrap());
-        assert_eq!(digit!(9), Digit::new(9).unwrap());
+        assert_eq!(digit!(0), Digit::Zero);
+        assert_eq!(digit!(1), Digit::One);
+        assert_eq!(digit!(2), Digit::Two);
+        assert_eq!(digit!(3), Digit::Three);
+        assert_eq!(digit!(4), Digit::Four);
+        assert_eq!(digit!(5), Digit::Five);
+        assert_eq!(digit!(6), Digit::Six);
+        assert_eq!(digit!(7), Digit::Seven);
+        assert_eq!(digit!(8), Digit::Eight);
+        assert_eq!(digit!(9), Digit::Nine);
+    }
+
+    #[test]
+    fn test_digit_add() {
+        for lhs in 0..10 {
+            for rhs in 0..10 {
+                let expected_res: Digit = ((lhs + rhs) % 10).try_into().unwrap();
+                let expected_carry: Digit = ((lhs + rhs) / 10).try_into().unwrap();
+
+                let lhs: Digit = lhs.try_into().unwrap();
+                let rhs: Digit = rhs.try_into().unwrap();
+
+                let (actual_res, actual_carry) = lhs + rhs;
+
+                assert_eq!(actual_res, expected_res);
+                assert_eq!(actual_carry, expected_carry);
+            }
+        }
+    }
+
+    #[test]
+    fn test_digit_div() {
+        for lhs in 0..10 {
+            for rhs in 1..10 {
+                let expected: Digit = (lhs / rhs).try_into().unwrap();
+
+                let lhs: Digit = lhs.try_into().unwrap();
+                let rhs: Digit = rhs.try_into().unwrap();
+
+                let actual = lhs / rhs;
+
+                assert_eq!(actual, expected);
+            }
+        }
+    }
+
+    #[test]
+    fn test_format() {
+        assert_eq!(Digit::Zero.to_string(), "0");
+        assert_eq!(Digit::One.to_string(), "1");
+        assert_eq!(Digit::Two.to_string(), "2");
+        assert_eq!(Digit::Three.to_string(), "3");
+        assert_eq!(Digit::Four.to_string(), "4");
+        assert_eq!(Digit::Five.to_string(), "5");
+        assert_eq!(Digit::Six.to_string(), "6");
+        assert_eq!(Digit::Seven.to_string(), "7");
+        assert_eq!(Digit::Eight.to_string(), "8");
+        assert_eq!(Digit::Nine.to_string(), "9");
+
+        assert_eq!(Digit::Zero.prettify(), "0");
+        assert_eq!(Digit::One.prettify(), "1");
+        assert_eq!(Digit::Two.prettify(), "2");
+        assert_eq!(Digit::Three.prettify(), "3");
+        assert_eq!(Digit::Four.prettify(), "4");
+        assert_eq!(Digit::Five.prettify(), "5");
+        assert_eq!(Digit::Six.prettify(), "6");
+        assert_eq!(Digit::Seven.prettify(), "7");
+        assert_eq!(Digit::Eight.prettify(), "8");
+        assert_eq!(Digit::Nine.prettify(), "9");
+    }
+
+    #[test]
+    fn test_from_str() {
+        assert_eq!(Digit::from_str("0").unwrap(), Digit::Zero);
+        assert_eq!(Digit::from_str("1").unwrap(), Digit::One);
+        assert_eq!(Digit::from_str("2").unwrap(), Digit::Two);
+        assert_eq!(Digit::from_str("3").unwrap(), Digit::Three);
+        assert_eq!(Digit::from_str("4").unwrap(), Digit::Four);
+        assert_eq!(Digit::from_str("5").unwrap(), Digit::Five);
+        assert_eq!(Digit::from_str("6").unwrap(), Digit::Six);
+        assert_eq!(Digit::from_str("7").unwrap(), Digit::Seven);
+        assert_eq!(Digit::from_str("8").unwrap(), Digit::Eight);
+        assert_eq!(Digit::from_str("9").unwrap(), Digit::Nine);
+
+        assert!(Digit::from_str("").is_err());
+        assert!(Digit::from_str("123").is_err());
+        assert!(Digit::from_str("a").is_err());
     }
 }
