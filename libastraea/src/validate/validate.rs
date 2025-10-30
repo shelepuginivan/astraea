92d1c8e1dfc301c70f210fbf9810002236ba37f5
shelepuginivan <shelepuginivanm@gmail.com>
2025-10-30 23:19:00 +0300

    docs(lib, validate): add rustdoc comments

--- libastraea/src/validate/validate.rs
+++ libastraea/src/validate/validate.rs
@@ -6,51 +6,13 @@ use crate::natural::NaturalNumber;
 use crate::polynomial::Polynomial;
 use crate::rational::RationalNumber;
 
+/// Ensures number of arguments provided to the instruction call.
 pub fn ensure_args_count(args: &Vec<String>, expected: usize) -> Result<(), InstructionError> {
     if args.len() != expected {
         return Err(InstructionError::count(expected, args.len()));
     }
 
     Ok(())
-}
-
-pub fn get_usize(args: &Vec<String>, arg_index: usize) -> Result<usize, InstructionError> {
-    let v = &args[arg_index];
-    usize::from_str(v).or_else(|_| {
-        Err(InstructionError::invalid_arg(
-            format!("cannot convert \"{}\" to usize", v),
-            arg_index,
-        ))
-    })
-}
-
-pub fn get_digit(args: &Vec<String>, arg_index: usize) -> Result<Digit, InstructionError> {
-    Digit::from_str(&args[arg_index])
-        .or_else(|e| Err(InstructionError::invalid_arg(e.message, arg_index)))
-}
-
-pub fn get_natural(
-    args: &Vec<String>,
-    arg_index: usize,
-) -> Result<NaturalNumber, InstructionError> {
-    NaturalNumber::from_str(&args[arg_index])
-        .or_else(|e| Err(InstructionError::invalid_arg(e.message, arg_index)))
-}
-
-pub fn get_rational(
-    args: &Vec<String>,
-    arg_index: usize,
-) -> Result<RationalNumber, InstructionError> {
-    RationalNumber::from_str(&args[arg_index])
-        .or_else(|e| Err(InstructionError::invalid_arg(e.message, arg_index)))
-}
-
-pub fn get_polynomial<T: Field>(
-    args: &Vec<String>,
-    arg_index: usize,
-) -> Result<Polynomial<T>, InstructionError> {
-    Polynomial::from_str(&args[arg_index])
-        .or_else(|e| Err(InstructionError::invalid_arg(e.message, arg_index)))
 }
 
 /// Parses arguments provided to the particular instruction and ensures their validity.
@@ -80,6 +42,51 @@ where
     Ok(res)
 }
 
+/// Parses argument at the specified index as usize.
+pub fn get_usize(args: &Vec<String>, arg_index: usize) -> Result<usize, InstructionError> {
+    let v = &args[arg_index];
+    usize::from_str(v).or_else(|_| {
+        Err(InstructionError::invalid_arg(
+            format!("cannot convert \"{}\" to usize", v),
+            arg_index,
+        ))
+    })
+}
+
+/// Parses argument at the specified index as Digit.
+pub fn get_digit(args: &Vec<String>, arg_index: usize) -> Result<Digit, InstructionError> {
+    Digit::from_str(&args[arg_index])
+        .or_else(|e| Err(InstructionError::invalid_arg(e.message, arg_index)))
+}
+
+/// Parses argument at the specified index as NaturalNumber.
+pub fn get_natural(
+    args: &Vec<String>,
+    arg_index: usize,
+) -> Result<NaturalNumber, InstructionError> {
+    NaturalNumber::from_str(&args[arg_index])
+        .or_else(|e| Err(InstructionError::invalid_arg(e.message, arg_index)))
+}
+
+/// Parses argument at the specified index as RationalNumber.
+pub fn get_rational(
+    args: &Vec<String>,
+    arg_index: usize,
+) -> Result<RationalNumber, InstructionError> {
+    RationalNumber::from_str(&args[arg_index])
+        .or_else(|e| Err(InstructionError::invalid_arg(e.message, arg_index)))
+}
+
+/// Parses argument at the specified index as Polynomial.
+pub fn get_polynomial<T: Field>(
+    args: &Vec<String>,
+    arg_index: usize,
+) -> Result<Polynomial<T>, InstructionError> {
+    Polynomial::from_str(&args[arg_index])
+        .or_else(|e| Err(InstructionError::invalid_arg(e.message, arg_index)))
+}
+
+/// Ensures 1 argument was provided and parses it to the corresponding type.
 pub fn one_arg<T>(args: Vec<String>) -> Result<T, InstructionError>
 where
     T: FromStr,
@@ -92,6 +99,7 @@ where
     Ok(first)
 }
 
+/// Ensures 2 arguments was provided and parses them to the corresponding type.
 pub fn two_args<T>(args: Vec<String>) -> Result<(T, T), InstructionError>
 where
     T: FromStr,
