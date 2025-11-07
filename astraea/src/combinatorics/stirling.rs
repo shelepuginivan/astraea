72ef942345e19de9879492e8146088aae8561ed5
shelepuginivan <shelepuginivanm@gmail.com>
2025-11-08 02:26:52 +0300

    feat(lib, combinatorics): Stirling number of the second kind

--- astraea/src/combinatorics/stirling.rs
+++ astraea/src/combinatorics/stirling.rs
@@ -3,9 +3,9 @@ use std::mem;
 use crate::algebra::{AddWithIdentity, MulWithIdentity};
 use crate::natural::NaturalNumber;
 
-/// Calculates Stirling number of first kind (unsigned).
+/// Calculates Stirling number of the first kind (unsigned).
 ///
-/// Stirling numbers of first kind equals to thehe number of permutations of n elements into k
+/// Stirling numbers of the first kind equals to the number of permutations of n elements into k
 /// cycles.
 ///
 /// ```
@@ -51,11 +51,65 @@ pub fn stirling_first_kind(n: &NaturalNumber, k: &NaturalNumber) -> NaturalNumbe
     current_row.swap_remove(k)
 }
 
+/// Returns nth row of the triangular array representation of the Stirling numbers of the second
+/// kind.
+fn stirling_second_kind_nth_row(n: usize) -> Vec<NaturalNumber> {
+    let mut current_row = vec![NaturalNumber::zero(); n + 1];
+    current_row[0] = NaturalNumber::one();
+
+    for i in 1..=n {
+        for k in (0..i + 1).rev() {
+            if k == 0 {
+                current_row[k] = NaturalNumber::zero();
+                continue;
+            }
+
+            let prev = mem::replace(&mut current_row[k], NaturalNumber::zero());
+            let col_index = NaturalNumber::from(k);
+            current_row[k] = current_row[k - 1].clone() + prev * col_index;
+        }
+    }
+
+    current_row
+}
+
+/// Calculates Stirling number of the second kind.
+///
+/// Stirling numbers of the second kind equals to the number of ways to partition a set with n
+/// elements into k non-empty subsets.
+///
+/// ```
+/// use astraea::combinatorics::stirling_second_kind;
+/// use astraea::natural::NaturalNumber;
+///
+/// let n = NaturalNumber::from(6_u8);
+/// let k = NaturalNumber::from(2_u8);
+/// let p = stirling_second_kind(&n, &k);
+///
+/// assert_eq!(p, NaturalNumber::from(31_u8));
+/// ```
+///
+/// Panics if n or k cannot be converted to usize.
+pub fn stirling_second_kind(n: &NaturalNumber, k: &NaturalNumber) -> NaturalNumber {
+    if n.is_zero() && k.is_zero() || n == k {
+        return NaturalNumber::one();
+    }
+
+    if k > n || n.is_zero() || k.is_zero() {
+        return NaturalNumber::zero();
+    }
+
+    let n: usize = n.clone().try_into().expect("value is too large");
+    let k: usize = k.clone().try_into().expect("value is too large");
+
+    stirling_second_kind_nth_row(n).swap_remove(k)
+}
+
 #[cfg(test)]
 mod tests {
     use crate::natural::NaturalNumber;
 
-    use super::stirling_first_kind;
+    use super::*;
 
     #[test]
     fn test_stirling_first_kind() {
@@ -81,6 +135,36 @@ mod tests {
 
                 let expected = NaturalNumber::from(expected);
                 let actual = stirling_first_kind(&n, &k);
+
+                assert_eq!(actual, expected, "mismatch for n={}, k={}", n, k);
+            }
+        }
+    }
+
+    #[test]
+    fn test_stirling_second_kind() {
+        #[rustfmt::skip]
+        let tests = vec![
+            vec![1_usize],
+            vec![0, 1],
+            vec![0, 1, 1],
+            vec![0, 1, 3,   1],
+            vec![0, 1, 7,   6,    1],
+            vec![0, 1, 15,  25,   10,    1],
+            vec![0, 1, 31,  90,   65,    15,    1],
+            vec![0, 1, 63,  301,  350,   140,   21,    1],
+            vec![0, 1, 127, 966,  1701,  1050,  266,   28,   1],
+            vec![0, 1, 255, 3025, 7770,  6951,  2646,  462,  36,  1],
+            vec![0, 1, 511, 9330, 34105, 42525, 22827, 5880, 750, 45, 1],
+        ];
+
+        for (n, row) in tests.into_iter().enumerate() {
+            for (k, expected) in row.into_iter().enumerate() {
+                let n = NaturalNumber::from(n);
+                let k = NaturalNumber::from(k);
+
+                let expected = NaturalNumber::from(expected);
+                let actual = stirling_second_kind(&n, &k);
 
                 assert_eq!(actual, expected, "mismatch for n={}, k={}", n, k);
             }
