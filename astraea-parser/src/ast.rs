ed848893d35a5df1805bbc447b71500c62b187c9
shelepuginivan <shelepuginivanm@gmail.com>
2026-04-29 01:32:17 +0300

    feat(parser): implement pre-order dfs iterator for ast

--- astraea-parser/src/ast.rs
+++ astraea-parser/src/ast.rs
@@ -1,3 +1,6 @@
+use std::collections::VecDeque;
+
+#[derive(Debug)]
 pub enum BinaryOp {
     Add,
     Sub,
@@ -5,6 +8,7 @@ pub enum BinaryOp {
     Div,
 }
 
+#[derive(Debug)]
 pub enum ASTNode {
     Literal {
         value: String,
@@ -23,7 +27,62 @@ pub enum ASTNode {
     },
 }
 
+pub struct PreOrderDFS<'a> {
+    stack: VecDeque<&'a ASTNode>,
+}
+
+impl<'a> PreOrderDFS<'a> {
+    pub fn new(root: &'a ASTNode) -> Self {
+        let mut stack = VecDeque::new();
+        stack.push_back(root);
+
+        Self { stack }
+    }
+}
+
+impl<'a> Iterator for PreOrderDFS<'a> {
+    type Item = &'a ASTNode;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let current = self.stack.pop_back()?;
+
+        match current {
+            ASTNode::BinaryOp { lhs, rhs, .. } => {
+                self.stack.push_back(&rhs);
+                self.stack.push_back(&lhs);
+            }
+            ASTNode::Function { args, .. } => {
+                self.stack.extend(args.iter().rev());
+            }
+            _ => {}
+        }
+
+        Some(current)
+    }
+}
+
 #[derive(Default)]
 pub struct AST {
     root: Option<ASTNode>,
+}
+
+impl AST {
+    pub fn new(root: ASTNode) -> Self {
+        Self { root: Some(root) }
+    }
+
+    pub fn prefix_notation(&self) -> String {
+        let result = String::new();
+
+        let pre_order_dfs = match self.root.as_ref() {
+            Some(root) => PreOrderDFS::new(&root),
+            None => return result,
+        };
+
+        for node in pre_order_dfs {
+            println!("{node:?}");
+        }
+
+        result
+    }
 }
