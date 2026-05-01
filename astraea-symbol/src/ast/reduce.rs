be2aa2635c23f431d258d0c3ac2b6050e223081a
shelepuginivan <shelepuginivanm@gmail.com>
2026-05-01 12:55:51 +0300

    feat(symbol): reduce literals

--- astraea-symbol/src/ast/reduce.rs
+++ astraea-symbol/src/ast/reduce.rs
@@ -1,8 +1,126 @@
+use std::ops::{Add, Div, Mul, Sub};
+
 use astraea::prelude::*;
 
 use super::node::{BinaryOp, Node};
 
 pub type ReduceFn<T> = fn(Node<T>) -> Node<T>;
+
+enum MaybeLiteralBinop<T: MathObject> {
+    Yes { operator: BinaryOp, lhs: T, rhs: T },
+    No(Node<T>),
+}
+
+impl<T: MathObject> From<Node<T>> for MaybeLiteralBinop<T> {
+    fn from(value: Node<T>) -> Self {
+        let (operator, lhs, rhs) = match value {
+            Node::BinaryOp { operator, lhs, rhs } => (operator, lhs, rhs),
+            _ => return Self::No(value),
+        };
+
+        let lhs = match *lhs {
+            Node::Literal(v) => v,
+            _ => return Self::No(Node::BinaryOp { operator, lhs, rhs }),
+        };
+
+        let rhs = match *rhs {
+            Node::Literal(v) => v,
+            _ => {
+                return Self::No(Node::BinaryOp {
+                    operator,
+                    lhs: Box::new(Node::Literal(lhs)),
+                    rhs,
+                });
+            }
+        };
+
+        return MaybeLiteralBinop::Yes { operator, lhs, rhs };
+    }
+}
+
+// Reduces binary addition of literals.
+pub fn reduce_literal_add<T>(node: Node<T>) -> Node<T>
+where
+    T: MathObject + Add<Output = T>,
+{
+    match MaybeLiteralBinop::from(node) {
+        MaybeLiteralBinop::No(n) => n,
+        MaybeLiteralBinop::Yes { operator, lhs, rhs } => {
+            if operator == BinaryOp::Add {
+                Node::Literal(lhs + rhs)
+            } else {
+                Node::BinaryOp {
+                    operator,
+                    lhs: Box::new(Node::Literal(lhs)),
+                    rhs: Box::new(Node::Literal(rhs)),
+                }
+            }
+        }
+    }
+}
+
+// Reduces binary subtraction of literals.
+pub fn reduce_literal_sub<T>(node: Node<T>) -> Node<T>
+where
+    T: MathObject + Sub<Output = T>,
+{
+    match MaybeLiteralBinop::from(node) {
+        MaybeLiteralBinop::No(n) => n,
+        MaybeLiteralBinop::Yes { operator, lhs, rhs } => {
+            if operator == BinaryOp::Sub {
+                Node::Literal(lhs - rhs)
+            } else {
+                Node::BinaryOp {
+                    operator,
+                    lhs: Box::new(Node::Literal(lhs)),
+                    rhs: Box::new(Node::Literal(rhs)),
+                }
+            }
+        }
+    }
+}
+
+// Reduces binary multiplication of literals.
+pub fn reduce_literal_mul<T>(node: Node<T>) -> Node<T>
+where
+    T: MathObject + Mul<Output = T>,
+{
+    match MaybeLiteralBinop::from(node) {
+        MaybeLiteralBinop::No(n) => n,
+        MaybeLiteralBinop::Yes { operator, lhs, rhs } => {
+            if operator == BinaryOp::Mul {
+                Node::Literal(lhs * rhs)
+            } else {
+                Node::BinaryOp {
+                    operator,
+                    lhs: Box::new(Node::Literal(lhs)),
+                    rhs: Box::new(Node::Literal(rhs)),
+                }
+            }
+        }
+    }
+}
+
+// Reduces binary division of literals.
+pub fn reduce_literal_div<T>(node: Node<T>) -> Node<T>
+where
+    T: MathObject + Div<Output = T>,
+{
+    match MaybeLiteralBinop::from(node) {
+        MaybeLiteralBinop::No(n) => n,
+        MaybeLiteralBinop::Yes { operator, lhs, rhs } => {
+            if operator == BinaryOp::Div {
+                Node::Literal(lhs / rhs)
+            } else {
+                Node::BinaryOp {
+                    operator,
+                    lhs: Box::new(Node::Literal(lhs)),
+                    rhs: Box::new(Node::Literal(rhs)),
+                }
+            }
+        }
+    }
+}
 
 // Reduces binary addition with additive identity.
 //
